d9f056f292bc8923b7dae206147ebc6430ec3004
MarciolimaDev <marclima998@gmail.com>
2024-03-04 12:37:06 -0500

    add basetemplate

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from recipe.views import home
+from . import views
 
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipe/<int:recipe_id>/', views.recipe)
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipe/pages/home.html')
+
+def recipe(request, recipe_id):
+    return render(request, 'recipe/pages/recipe_view.html')
