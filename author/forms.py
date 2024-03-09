79cac85a206a40c0fa2b3400c6e7cb6f52191972
MarciolimaDev <marclima998@gmail.com>
2024-03-09 14:04:54 -0500

    add forms pt1

--- author/forms.py
+++ author/forms.py
@@ -1,10 +1,21 @@
+from cProfile import label
 from dataclasses import fields
 from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+            'placeholder':'Confirme sua senha'
+        }),
+        label='Confirme sua senha')
+
     class Meta:
         model = User
+
         fields = [
             'first_name',
             'last_name',
@@ -12,6 +23,27 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password'
         ]
+
+        labels = {
+            'first_name':'Nome',
+            'last_name':'Sobrenome',
+            'username':'Nome de usuário',
+            'email':'Email',
+            'password':'Senha'
+        }
+
+        help_texts = {
+            'username':'Nome de usuário que será usado para fazer login',
+            'email':'Email que será usado para fazer login'
+        }
+
+        widgets = {
+            'password':forms.PasswordInput(attrs={
+                'placeholder':'Digite sua senha'
+            }),
+        }
+
+        
 
 
     
--- author/views.py
+++ author/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
 from .forms import RegisterForm
+from django.http import Http404
 
 # Create your views here.
 def register(request):
 
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+
+    form = RegisterForm(register_form_data)
+
     return render(request, 'author/pages/register.html', {
         'form':form
         }
     )
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404('Only POSTs are allowed')
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
